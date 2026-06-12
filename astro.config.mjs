eed33221870044c2df2d04c67fcf845f1025b250
projectanshhh <projectanshhh@users.noreply.github.com>
2026-06-12 23:06:47 +0530

    fix: remove astro base path to fix vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://projectanshhh.github.io',
-  base: '/campusx-ai-tracker/',
   vite: {
     plugins: [tailwindcss()],
   },
